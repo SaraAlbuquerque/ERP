4779cf8decb23a1728df54775295122f1dbce1b3
Keusan <sara.albuquerque1503@gmail.com>
2022-02-15 06:17:17 -0300

    CRUD usuario

--- src/main/java/model/bean/usuario.java
+++ src/main/java/model/bean/usuario.java
@@ -15,4 +15,46 @@ public class usuario {
     private String password;
     private String recoveryAnswer;
     private String recoveryEmail;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRecoveryAnswer() {
+        return recoveryAnswer;
+    }
+
+    public void setRecoveryAnswer(String recoveryAnswer) {
+        this.recoveryAnswer = recoveryAnswer;
+    }
+
+    public String getRecoveryEmail() {
+        return recoveryEmail;
+    }
+
+    public void setRecoveryEmail(String recoveryEmail) {
+        this.recoveryEmail = recoveryEmail;
+    }
+    
+    
 }
--- src/main/java/model/dao/usuarioDAO.java
+++ src/main/java/model/dao/usuarioDAO.java
@@ -8,6 +8,14 @@ package model.dao;
 import connection.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.bean.usuario;
 
 /**
  *
@@ -15,7 +23,106 @@ import java.sql.PreparedStatement;
  */
 public class usuarioDAO {
 
-    Connection con = ConnectionFactory.getConnection();
-    PreparedStatement stmt = null;
+    public void create(usuario u){
+        
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+            
+        try {
+            stmt = con.prepareStatement("INSERT INTO usuario (userName, userPassword, recoveryAnswer, recoveryEmail) VALUES (?,?,?,?)");
+            stmt.setString(1, u.getUserName());
+            stmt.setString(2, u.getPassword());
+            stmt.setString(3, u.getRecoveryAnswer());
+            stmt.setString(4, u.getRecoveryEmail());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao salvar:" + ex);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
     
+    public List<usuario> read(){
+        
+        
+            Connection con = ConnectionFactory.getConnection();
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+            List<usuario> usuarios = new ArrayList();
+            
+        try {
+            stmt = con.prepareStatement("SELECT * FROM usuario");
+            rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                
+                usuario user = new usuario();
+                
+                user.setUserId(rs.getInt("userId"));
+                user.setUserName(rs.getString("userName"));
+                user.setPassword(rs.getString("password"));
+                user.setRecoveryAnswer(rs.getString("recoveryAnswer"));
+                user.setRecoveryEmail(rs.getString("recoveryEmail"));
+                
+                usuarios.add(user);
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(usuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        
+        return usuarios;
+    }
+    
+    public void update(usuario u){
+        
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+            
+        try {
+            stmt = con.prepareStatement("DELETE FROM usuario WHERE userId = ?");
+            stmt.setInt(1, u.getUserId());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao excluir:" + ex);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    
+    public void delete(usuario u){
+        
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+            
+        try {
+            stmt = con.prepareStatement("UPDATE usuario SET userName = ?, userPassword = ?, recoveryAnswer = ?, recoveryEmail = ? WHERE userId = ?");
+            stmt.setString(1, u.getUserName());
+            stmt.setString(2, u.getPassword());
+            stmt.setString(3, u.getRecoveryAnswer());
+            stmt.setString(4, u.getRecoveryEmail());
+            stmt.setInt(5, u.getUserId());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Atualizado com sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao atualizar:" + ex);
+        } finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+    
+      
 }
